10b2d283c07198e92c75675f17c96658e87c8f4c
Arseniy Latyshev <landih73@gmail.com>
2022-10-02 21:25:45 +0300

    GEN-1 Структура проекта

--- app/support.rb
+++ app/support.rb
@@ -2,7 +2,7 @@
 
 module Support
   DEFAULT_SYMBOL = '=>'
-  DEFAULT_PASSWORD_STORAGE = '/home/work/work_dir_1/pass_gen/passwords.txt'
+  DEFAULT_PASSWORD_STORAGE = '/home/work/work_dir_1/pass_gen/lib/passwords.txt'
 
   def self.new_line(service, password)
     [service, DEFAULT_SYMBOL, password].join(' ')
